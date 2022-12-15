7ce0d1705a2a4b2df01b7ccff3dac5899e6cba02
ARY <widhiarya@outlook.com>
2022-12-15 15:16:42 +0700

    initial commit + bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { GaleriComponent } from './galeri/galeri.component';
+import { GaleriActiveComponent } from './galeri-active/galeri-active.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { PbbComponent } from './pbb/pbb.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GaleriComponent,
+    GaleriActiveComponent,
+    JumbotronComponent,
+    PbbComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
